8e169e61534c3d2226e68ae76bb02abc4c3c05cb
eusebiuuu <107063753+eusebiuuu@users.noreply.github.com>
2024-07-10 19:45:28 +0300

    Update fast-power.cpp

--- templates/fast-power.cpp
+++ templates/fast-power.cpp
@@ -1,31 +1,32 @@
 int const mod = 998244353, N = 1e6 + 6;
 int fact[N];
 
-
 int mul(int a, int b) {
-   return 1LL * a * b % mod;
+    if (a < 0) a += mod;
+    if (b < 0) b += mod;
+    return 1LL * a * b % mod;
 }
-
 
 int add(int a, int b) {
-   return (a + b) % mod;
+    if (a < 0) a += mod;
+    if (b < 0) b += mod;
+    return (a + b) % mod;
 }
 
-
 int fastPow(int base, int exp) {
-   int ans = 1;
-   while (exp > 0) {
-       if (exp & 1) {
-           ans = mul(ans, base);
-       }
-       base = mul(base, base);
-       exp >>= 1;
-   }
-   return ans;
+    int ans = 1;
+    while (exp > 0) {
+        if (exp & 1) {
+            ans = mul(ans, base);
+        }
+        base = mul(base, base);
+        exp >>= 1;
+    }
+    return ans;
 }
 
 int inv(int num) {
-   return fastPow(num, mod - 2);
+    return fastPow(num, mod - 2);
 }
 
 int nCr(int n, int r) {
