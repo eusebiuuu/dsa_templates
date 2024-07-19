73f0307a7c1c6059e1df35fa5c92ef966dc7a042
eusebiuuu <107063753+eusebiuuu@users.noreply.github.com>
2024-07-19 13:39:03 +0300

    Update matrix-expo.cpp

--- templates/matrix-expo.cpp
+++ templates/matrix-expo.cpp
@@ -1,36 +1,34 @@
+int const M = 1e9 + 7;
+
 template <int mod>
-class Matrix
-{
+class Matrix {
 private:
   vector<vector<int>> m;
 
 public:
-  Matrix(int rows, int cols)
-  {
+  Matrix(int rows, int cols) {
     m.resize(rows, vector<int>(cols));
   }
 
-  void setNeutral()
-  {
-    for (int i = 0; i < (int)m.size(); ++i)
-    {
+  void setNeutral() {
+    for (int i = 0; i < (int) m.size(); ++i) {
       m[i][i] = 1;
     }
   }
 
   int getRows() {
-    return (int)m.size();
+    return (int) m.size();
   }
 
   int getCols() {
-    return (int)m[0].size();
+    return (int) m[0].size();
   }
 
-  int add(int a, int b) {
+  static int add(int a, int b) {
     return (a + b) % mod;
   }
 
-  int mul(int a, int b) {
+  static int mul(int a, int b) {
     return 1LL * a * b % mod;
   }
 
@@ -56,24 +54,19 @@ public:
 
   Matrix operator+(const Matrix &curMatrix) {
     Matrix res(getRows(), getCols());
-    for (int i = 0; i < (int)m.size(); ++i)
-    {
-      for (int j = 0; j < (int)m[0].size(); ++j)
-      {
+    for (int i = 0; i < (int)m.size(); ++i) {
+      for (int j = 0; j < (int)m[0].size(); ++j) {
         res.m[i][j] = add(m[i][j], curMatrix.m[i][j]);
       }
     }
     return res;
   }
-  Matrix operator^(long long exp)
-  {
+  Matrix operator^(long long exp) {
     Matrix ans(getRows(), getCols()), base(getRows(), getCols());
     ans.setNeutral();
     base.m = this->m;
-    while (exp > 0)
-    {
-      if (exp & 1)
-      {
+    while (exp > 0) {
+      if (exp & 1){
         ans = ans * base;
       }
       base = base * base;
