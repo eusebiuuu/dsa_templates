111f404f32eb8a135dfa01322de1c4c93390026f
eusebiuuu <107063753+eusebiuuu@users.noreply.github.com>
2024-09-07 19:45:29 +0300

    Update kmp-failure-function.cpp

--- templates/kmp-failure-function.cpp
+++ templates/kmp-failure-function.cpp
@@ -1,23 +1,13 @@
-int const N = 5e5 + 5;
-int len[N];
-// s este indexat de la 0
-void failureFunction(string s)
-{
-  int j = 0, i = 1;
-  while (i < s.size())
-  {
-    if (s[i] == s[j])
-    {
-      len[i++] = ++j;
+void failure_function(string s, vector<int> &len) {
+    int j = 0, i = 1, sz = s.size();
+    while (i < sz) {
+        if (s[i] == s[j]) {
+            len[i++] = ++j;
+        } else if (j == 0) {
+            len[i++] = 0;
+        } else {
+            j = len[j - 1];
+        }
     }
-    else if (j == 0)
-    {
-      len[i++] = 0;
-    }
-    else
-    {
-      j = len[j - 1];
-    }
-  }
 }
-// Len[i] reprezinta lungimea celui mai lung prefix adecvat care este și sufix pentru s[0, i]
+// len[i] - the length of the longest proper prefix that is also suffix for s[0, i]
