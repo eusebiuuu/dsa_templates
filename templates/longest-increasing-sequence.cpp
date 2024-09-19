00955626d2f63ecc9640fa9020b584ecea2ac797
eusebiuuu <107063753+eusebiuuu@users.noreply.github.com>
2024-09-19 20:05:49 +0300

    Update longest-increasing-sequence.cpp

--- templates/longest-increasing-sequence.cpp
+++ templates/longest-increasing-sequence.cpp
@@ -1,22 +1,20 @@
-int lis(vector<int> a)
-{
-  int n = a.size();
-  const int INF = 1e9;
-  vector<int> d(n + 1, INF);
-  d[0] = -INF;
+int get_LIS(vector<int> a) {
+    int n = a.size();
+    vector<int> d(n + 1, INF);
+    d[0] = -INF;
 
-  for (int i = 0; i < n; i++)
-  {
-    int j = upper_bound(d.begin(), d.end(), a[i]) - d.begin();
-    if (d[j - 1] < a[i] && a[i] < d[j])
-      d[j] = a[i];
-  }
+    for (int i = 0; i < n; i++) {
+        int j = upper_bound(d.begin(), d.end(), a[i]) - d.begin();
+        if (d[j - 1] < a[i]) {
+            d[j] = a[i];
+        }
+    }
 
-  int ans = 0;
-  for (int i = 0; i <= n; i++)
-  {
-    if (d[i] < INF)
-      ans = i;
-  }
-  return ans;
+    int ans = 0;
+    for (int i = 0; i <= n; i++) {
+        if (d[i] < INF) {
+            ans = i;
+        }
+    }
+    return ans;
 }
