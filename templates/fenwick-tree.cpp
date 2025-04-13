0fbf35d7e858439b751582807e9fb9ed2c0994a1
eusebiuuu <107063753+eusebiuuu@users.noreply.github.com>
2025-04-13 09:49:40 +0300

    Update fenwick-tree.cpp

--- templates/fenwick-tree.cpp
+++ templates/fenwick-tree.cpp
@@ -1,41 +1,34 @@
-class FenwickTree
-{
-  // range queries and updates (binary, identity, inverse): https://cp-algorithms.com/data_structures/fenwick.html
-  // Don’t forget to build the tree with updates, not with assignments
-  // Change update and query accordingly
-  // Change initial value of carry
-  // Change type if needed
-  // Index from 0 if needed
-private:
-  int size;
-  vector<int> tree;
+class FenwickTree {
+    // range queries and updates (binary, identity, inverse):
+    // https://cp-algorithms.com/data_structures/fenwick.html Don’t forget to
+    // build the tree with updates, not with assignments Change update and query
+    // accordingly Change initial value of carry Change type if needed Index
+    // from 0 if needed
+   private:
+    int size;
+    vector<int> tree;
 
-public:
-  FenwickTree(int curSize, int value)
-  {
-    this->size = curSize;
-    tree.resize(curSize + 1, value);
-  }
-
-  void update(int pos, int value)
-  {
-    while (pos <= this->size)
-    {
-      this->tree[pos] += value;
-      pos += pos & -pos;
-      // pos |= pos + 1;
+   public:
+    FenwickTree(int curSize, int value) {
+        this->size = curSize;
+        tree.resize(curSize + 1, value);
     }
-  }
 
-  int query(int rightBound)
-  {
-    int carry = 0;
-    while (rightBound > 0)
-    {
-      carry += this->tree[rightBound];
-      rightBound -= rightBound & -rightBound;
-      // rightBound = (rightBound & (rightBound + 1)) - 1;
+    void update(int pos, int value) {
+        while (pos <= this->size) {
+            this->tree[pos] += value;
+            pos += pos & -pos;
+            // pos |= pos + 1;
+        }
     }
-    return carry;
-  }
+
+    int query(int rightBound) {
+        int carry = 0;
+        while (rightBound > 0) {
+            carry += this->tree[rightBound];
+            rightBound -= rightBound & -rightBound;
+            // rightBound = (rightBound & (rightBound + 1)) - 1;
+        }
+        return carry;
+    }
 };
