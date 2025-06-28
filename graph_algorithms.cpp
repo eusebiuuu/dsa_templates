8e3f3561765b3ba1ca5cd8a6ce6c81e894a1ea20
Eusebiu <eusebiu.rimboi04@gmail.com>
2025-06-29 00:26:12 +0300

    Refactor structure and filenames

--- graph_algorithms.cpp
+++ graph_algorithms.cpp
@@ -545,43 +545,6 @@ class GraphToolkit {
         return cycle;
     }
 
-    // len[i] - the length of the longest proper prefix that is also suffix for
-    // s[0, i]
-    void failure_function(string s, vector<int>& len) {
-        int j = 0, i = 1, sz = s.size();
-        while (i < sz) {
-            if (s[i] == s[j]) {
-                len[i++] = ++j;
-            } else if (j == 0) {
-                len[i++] = 0;
-            } else {
-                j = len[j - 1];
-            }
-        }
-    }
-
-    vector<int> zFunction(string s) {
-        int l = 0, r = 0, idx = 1, sz = s.size();
-        vector<int> maxLen(sz);
-        while (idx < sz) {
-            if (idx < r) {
-                maxLen[idx] = min(r - idx, maxLen[idx - l]);
-            }
-            while (idx + maxLen[idx] < sz and
-                   s[idx + maxLen[idx]] == s[maxLen[idx]]) {
-                maxLen[idx]++;
-            }
-            if (idx + maxLen[idx] > r) {
-                l = idx;
-                r = idx + maxLen[idx];
-            }
-            idx++;
-        }
-        return maxLen;
-    }
-    // maxLen[i] = lungimea celui mai lung prefix pentru sirul s care incepe pe
-    // pozitia i
-
     int min_cost_flow(int N, vector<Edge2> edges, int K, int s, int t) {
         vector<vector<int>> adj, cost, capacity;
 
--- others.cpp
+++
@@ -1,6 +0,0 @@
-mt19937 rnd(chrono::steady_clock::now().time_since_epoch().count());
-
-#ifdef ONPC
-cerr << '\n'
-     << "Program finished in" << clock() * 1.0 / CLOCKS_PER_SEC << "sec" << '\n';
-#endif
